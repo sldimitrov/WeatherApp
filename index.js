cc2feb05abab6e4184d80cbd303fc35635662165
Slavi Dimitrov <slavi@orbisds.com>
2024-04-15 13:07:53 +0300

    add left-side-bar and a button for it to pop-up when clicked

--- index.js
+++ index.js
@@ -2,31 +2,52 @@ const apiKey = "539d9b287b9b99eee7e12081aa43f3a8";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search-bar-container input");
-const searchBtn = document.querySelector(".search-bar-container button");
+const searchBtn = document.getElementById("search-button");
+const historyBtn = document.getElementById("history-button")
 const weatherIcon = document.getElementById("cloud-img")
 const miniIcon = document.querySelector(".weather-img")
+
+historyBtn.addEventListener('click', showHistory)
+
+async function showHistory(){
+    /* This function should show a slide bar that appears from the left of the page */
+    document.querySelector('.sidebar-container').classList.toggle('active');
+} 
+
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
-    document.getElementById("city").innerHTML = data.name;
-    document.querySelector(".current-temperature").innerHTML = Math.round(data.main.temp) + "°";
+    var city = data.name;
+    var currentTemperature = Math.round(data.main.temp) + "°";
+    var forecastHeading = data.weather[0].description;
+    var tempMax = Math.round(data.main.temp_max) + "°c";
+    var tempMin = Math.round(data.main.temp_min) + "°c";
+    var humidity = data.main.humidity + "%";
+    var clouds = data.clouds.all + "%";
+    var wind = data.wind.speed + " km/h";
+    
+    var weatherDescription = data.weather[0].description;
+    var forecastTemp = Math.round(data.main.temp) + "°c";
+
+    document.getElementById("city").innerHTML = city;
+    document.querySelector(".current-temperature").innerHTML = currentTemperature;
 
     const myUnixTimestamp = data.dt; // start with a Unix timestamp
     const myDate = new Date(myUnixTimestamp * 1000).toUTCString(); // convert timestamp to milliseconds and construct Date object
 
     document.getElementById("date").innerHTML = myDate;
-    document.getElementById("forecast-heading").innerHTML = data.weather[0].description;
+    document.getElementById("forecast-heading").innerHTML = forecastHeading;
 
-    document.getElementById("temp-max").innerHTML = Math.round(data.main.temp_max) + "°c";
-    document.getElementById("temp-min").innerHTML = Math.round(data.main.temp_min) + "°c";
-    document.getElementById("humidity").innerHTML = data.main.humidity + "%";
-    document.getElementById("cloudy").innerHTML = data.clouds.all + "%";
-    document.getElementById("wind").innerHTML = data.wind.speed + " km/h";
+    document.getElementById("temp-max").innerHTML = tempMax;
+    document.getElementById("temp-min").innerHTML = tempMin;
+    document.getElementById("humidity").innerHTML = humidity;
+    document.getElementById("cloudy").innerHTML = clouds;
+    document.getElementById("wind").innerHTML = wind;
 
-    document.getElementById("weather-description").innerHTML = data.weather[0].description;
-    document.querySelector(".forecast-temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.getElementById("weather-description").innerHTML = weatherDescription;
+    document.querySelector(".forecast-temp").innerHTML = forecastTemp;
 
     const mapper = new Map([
         ["Clouds", "images/clouds.png"],
@@ -36,12 +57,19 @@ async function checkWeather(city){
         ["Mist", "images/mist.png"],
       ]);
 
-      data = data.weather[0].main;
+    forecastData = data.weather[0].main;
 
-      icon = mapper.get(data)
+    icon = mapper.get(forecastData)
 
-      weatherIcon.src = icon;
-      miniIcon.src = icon;
+    weatherIcon.src = icon;
+    miniIcon.src = icon;
+
+    // After visualisation we will need to store the 
+    // searched data into the session storage
+    
+    sessionStorage.setItem('city', JSON.stringify({'name': city}));
+    // Check the saved data into the sessionStorage 
+    // console.log(sessionStorage);
 }
 
 searchBtn.addEventListener("click", ()=>{
